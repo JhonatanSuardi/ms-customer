cf1448b882ff5448b832dd2204a6e00d17f27636
vithor97 <vitorsantos27@hotmail.com>
2022-01-28 11:19:31 -0300

    uri builder created method controller custommers

--- src/main/java/com/financials/customer/api/controller/CustomerController.java
+++ src/main/java/com/financials/customer/api/controller/CustomerController.java
@@ -7,10 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -40,9 +41,11 @@ public class CustomerController {
 
     @PostMapping
     @Validated
-    public ResponseEntity<Customer> creatCustomer(@RequestBody @Valid final CustomerDto customerDto) {
+    public ResponseEntity<Customer> creatCustomer(@RequestBody @Valid final CustomerDto customerDto, UriComponentsBuilder uriBuilder) {
         Customer createdCustomer = customerService.insertCustomer(customerDto);
-        return ResponseEntity.ok(createdCustomer);
+        UriComponents uri = uriBuilder.path("/custommers/{uuid}").buildAndExpand(createdCustomer.getUuid());
+
+        return ResponseEntity.created(uri.toUri()).body(createdCustomer);
     }
 
     @PatchMapping("/{patchId}")
